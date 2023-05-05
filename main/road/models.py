c82f7c3dae23bf8df7ebbf91eecc9ac543cde3e0
Beksultan Almazbekov <anime.beksultan@gmail.com>
2023-05-05 15:52:32 +0600

    first comit

--- main/road/models.py
+++ main/road/models.py
@@ -1,5 +1,27 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+RATING_CHOICES = ((1, 1),
+                  (2, 2),
+                  (3, 3),
+                  (4, 4),
+                  (5, 5))
 
-# Create your models here.
+# IMPORTANCE_CHOICES = (('Федеративное', 'Федеративное'),
+#                       ('Областное', 'Областное'),
+#                       ('Городское', 'Городское'),
+#                       ('Сельское', 'Сельское')
+#                       )
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='')
+    street = models.CharField(max_length=100, null=True)
+    rating = models.PositiveSmallIntegerField(choices=RATING_CHOICES)
+    description = models.TextField(null=True, blank=True)
+    importance = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
